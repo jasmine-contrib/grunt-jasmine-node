b938cc93a95a6702b825e5c1f6336b8986db4f6f
Omar Gonzalez <omar@almerblank.com>
2012-09-01 13:06:54 -0700

    Initial commit of grunt-jasmine-node v0.0.1

---
+++ tasks/jasmine-node-task.js
@@ -0,0 +1,71 @@
+module.exports = function (grunt) {
+
+    grunt.registerTask("jasmine_node", "Runs jasmine-node.", function() {
+      var jasmine = require('jasmine-node');
+      var util;
+      var Path = require('path');
+
+      try {
+          util = require('util');
+      } catch(e) {
+          util = require('sys');
+      }
+
+      var projectRoot     = grunt.config("jasmine_node.projectRoot") || ".";
+      var source          = grunt.config("jasmine_node.source") || "src";
+      var specFolderName  = grunt.config("jasmine_node.specFolderName") || "spec";
+      var isVerbose       = grunt.config("jasmine_node.verbose") || true;
+      var showColors      = grunt.config("jasmine_node.colors") || true;
+      var teamcity        = grunt.config("jasmine_node.teamcity") || false;
+      var useRequireJs    = grunt.config("jasmine_node.requirejs") || false;
+      var extensions      = grunt.config("jasmine_node.extensions") || "js";
+      var match           = grunt.config("jasmine_node.match") || ".";
+      var matchall        = grunt.config("jasmine_node.matchall") || false;
+      var autotest        = grunt.config("jasmine_node.autotest") || false;
+      var useHelpers      = grunt.config("jasmine_node.useHelpers") || false;
+      var forceExit       = grunt.config("jasmine_node.forceExit") || false;
+
+      var junitreport = {
+          report: false,
+          savePath : "./reports/",
+          useDotNotation: true,
+          consolidate: true
+      };
+
+      var jUnit = grunt.config("jasmine_node.jUnit") || junitreport;
+
+      // Tell grunt this task is asynchronous.
+      var done = this.async();
+
+      var regExpSpec = new RegExp(match + (matchall ? "" : specFolderName + "\\.") + "(" + extensions + ")$", 'i');
+      var onComplete = function(runner, log) {
+        var exitCode;
+        util.print('\n');
+        if (runner.results().failedCount === 0) {
+          exitCode = 0;
+        } else {
+          exitCode = 1;
+
+          if (forceExit) {
+            process.exit(exitCode);
+          }
+        }
+
+        done();
+      };
+
+      try {
+        jasmine.executeSpecsInFolder(projectRoot,
+          onComplete,
+          isVerbose,
+          showColors,
+          teamcity,
+          useRequireJs,
+          regExpSpec,
+          jUnit);
+      }
+      catch (e) {
+        console.log(e);
+      }
+    });
+};
